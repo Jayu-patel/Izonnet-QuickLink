aaa1cea02602bf065493a394010f8818b933539d
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2025-07-14 17:48:19 +0530

    cookie added

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -2,5 +2,12 @@ const generateToken = require("../utils/generate");
 
 exports.googleCallback = (req, res) => {
   const token = generateToken(req.user);
+  res.cookie(
+      'temp_auth_token', 
+      token,
+      {
+          sameSite: 'Lax', httpOnly: false, secure: false, maxAge: 1000 * 60 * 60 * 24 * 7,
+      }
+  )
   res.redirect(`http://localhost:5173?token=${token}`);
 };
