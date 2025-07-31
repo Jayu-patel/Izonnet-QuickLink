1549748c0f91e987173fc5a0836abae167c494c6
Jay Patel <96169579+Jayu-patel@users.noreply.github.com>
2025-07-31 11:22:29 +0530

    google auth error fix #1

--- server/config/passport.js
+++ server/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: `${process.env.BASE_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       let user = await User.findOne({ email: profile.emails[0].value });
